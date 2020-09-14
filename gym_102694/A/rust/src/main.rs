c4418c2d5aeacb4afeabf4700a72763c85e94898
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-14 16:05:11 +0300

    Commiting a wrong algortihm.

--- gym_102694/A/rust/src/main.rs
+++ gym_102694/A/rust/src/main.rs
@@ -14,10 +14,8 @@ fn read_line_of_i32s() -> Vec<i32> {
 
 fn main() {
     let n = read_line_of_i32s()[0];
-    // println!("{}", n);
-
+    println!("{}", n);
     let mut tree: Vec<(i32, i32)> = vec![];
-
     for i in 0..n - 1 {
         // read the whole line
         let pair = read_line_of_i32s();
@@ -28,86 +26,117 @@ fn main() {
             )
         );
     }
-
     tree.sort_by(
         |a, b| {
             a.0.cmp(&b.0)
         }
     );
-
-    /*
-    println!(
-        "{:?}",
-        tree
-    );
-     */
-
     if tree.is_empty() {
         println!("0");
         return;
     }
-
+    println!(
+        "{:?}",
+        tree
+    );
     let mut bfs: std::collections::VecDeque::<i32> =
         std::collections::VecDeque::new();
     let mut vis: std::collections::HashSet::<i32> =
         std::collections::HashSet::new();
-
     vis.insert(
         tree[0].0
     );
     bfs.push_back(
         tree[0].0
     );
-
     let mut level: i32 = 0;
-    let mut recent_lvls: Vec<Vec<i32>> = vec![vec![tree[0].0]];
-    recent_lvls.push(vec![]);
-
+    let mut parent = std::collections::HashMap::<i32, i32>::new();
     while !bfs.is_empty() {
         let x = bfs.pop_front().unwrap();
         vis.insert(x);
-        // println!("just visited {}", x);
-        let mut advance = false;
         for i in &tree {
             if i.0 == x && !vis.contains(
                 &i.1
             ) {
-                // println!("!val: {}", i.1);
                 bfs.push_back(i.1);
                 vis.insert(i.1);
-                recent_lvls.last_mut().unwrap().push(i.1);
-                advance = true;
+                parent.insert(
+                    i.1,
+                    x,
+                );
             }
         }
-        if advance == true {
-            recent_lvls.push(vec![]);
-        }
     }
-    recent_lvls.pop();
-    /*
     println!(
         "{:?}",
-        recent_lvls
+        parent
     );
-     */
-    // can get the last 2 levels
-    let last = &recent_lvls[recent_lvls.len() - 1];
-    let pre_last = &recent_lvls[recent_lvls.len() - 2];
-    /*
-    println!(
-        "{:?}, {:?}",
-        pre_last,
-        last
-    );
-     */
+    vis = std::collections::HashSet::new();
+    let mut reach = std::collections::HashMap::<i32, i32>::new();
+    for (start_from, _) in &tree {
+        println!(
+            "started from {}",
+            start_from
+        );
+        let mut e = *start_from;
+        let mut trail: Vec<i32> = vec![];
+        while parent.contains_key(&e) && !vis.contains(&e) {
+            trail.push(e);
+            vis.insert(e);
+            e = parent[&e];
+        }
+        println!(
+            "reach {:?}",
+            reach
+        );
+        println!("the trail {:?}", trail);
 
-    if last.len() > 1 {
-        println!("{}",
-                 (recent_lvls.len() - 1) * 2
+        // if root
+        if trail.len() == 0 {
+            reach.insert(e, 0);
+        }
+
+        // traverse the trail & fill in the gaps
+        for (ind, rval) in trail.iter().rev().enumerate() {
+            println!(
+                "ind {} : rval {}",
+                ind, rval
+            );
+            if !reach.contains_key(rval) {
+                reach.insert(*rval, 0);
+            }
+            println!(
+                "adding {} and {} to {}",
+                ind + 1,
+                reach[&parent[trail.last().unwrap()]],
+                rval,
+            );
+            *reach.get_mut(rval).unwrap()
+                += reach[&parent[trail.last().unwrap()]]
+                + ind as i32 + 1;
+        }
+    }
+    println!(
+        "{:?}",
+        reach
+    );
+
+    println!(
+        "the reach {:?}",
+        reach
+    );
+
+    let capacity = reach.iter().len();
+    if capacity > 1 {
+        println!(
+            "{}",
+            reach.values().nth(capacity - 1)
+                + reach.values().nth(capacity - 2)
         );
     } else {
-        println!("{}",
-                 (recent_lvls.len() - 1) * 2 - 1
+        println!(
+            "{}",
+
         );
     }
 }
