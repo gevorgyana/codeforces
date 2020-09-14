d47e00a96ca19929dd70d49152fcf98a9bdc7a51
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-14 20:54:21 +0300

    Update. Just store the leaves, and the distances from the leaves to the root. Bfs does just that. So start moving from the root, incrementing the level counter, then go down until you reach your leaves, and when you do that, collect them to a vector of some kind. When you finish, you can iterate over the leaves and see how distant they are from the root. Update the 2 greatest values and return their sum as the diameter of the tree. If one of them is 0, then there is only 1 leaf.

--- gym_102694/A/rust/src/main.rs
+++ gym_102694/A/rust/src/main.rs
@@ -14,20 +14,20 @@ fn read_line_of_i32s() -> Vec<i32> {
 
 fn main() {
     let n = read_line_of_i32s()[0];
-    println!("{}", n);
     let mut tree: Vec<(i32, i32)> = vec![];
     for i in 0..n - 1 {
         // read the whole line
         let pair = read_line_of_i32s();
-        tree.push(
-            (
-                pair[0],
-                pair[1]
-            )
-        );
+        tree.push((pair[0], pair[1]));
+        tree.push((pair[1], pair[0]));
     }
 
-    /* computationally no reason to sort unless you have upper/lower
+    if n == 1 {
+        println!("0");
+        return;
+    }
+
+    /* Computationally no reason to sort unless you have upper/lower
      * bounds which I do not have in Rust, and I don't want to implement
      * that. So just use linear search thru the whole array, or allocate
      * memory in advance (which is another thing I don't want to do).
@@ -38,83 +38,62 @@ fn main() {
     );
      */
 
-    // run a bfs and identify parents and leaves
+    // Run a bfs and identify leaves
     let mut bfs = std::collections::VecDeque::<i32>::new();
-    let mut parent = std::collections::HashMap::<i32, i32>::new();
     let mut vis = std::collections::HashSet::<i32>::new();
     let mut leaves: Vec<i32> = vec![];
+    let mut node_level = std::collections::HashMap::<i32, i32>::new();
     let root = tree[0].0;
-    bfs.push_back(
-        root
-    );
+    node_level.insert(root, 0);
+    bfs.push_back(root);
     vis.insert(root);
+    let mut level = 1;
     while !bfs.is_empty() {
         let current = bfs.pop_front().unwrap();
         let frozen_len = bfs.len();
         for c in &tree {
             if c.0 == current && !vis.contains(&c.1) {
-                println!("{} -> {}", current, c.1);
+                // println!("{} -> {}", current, c.1);
                 vis.insert(c.1);
                 bfs.push_back(c.1);
-                parent.insert(
-                    c.1, c.0
+                node_level.insert(c.1, level);
+                /*
+                println!("assign {} to {}",
+                         level, c.1
                 );
+                 */
             }
         }
         if bfs.len() == frozen_len {
-            leaves.push(
-                current
-            );
+            leaves.push(current);
         }
+        level += 1;
     }
-    vis = std::collections::HashSet::new();
+    /*
     println!(
         "{:?}",
         leaves
     );
-
-    // distance from a node to the root
-    let mut local_dist = std::collections::HashMap::<i32, i32>::new();
+    println!("{:?}",
+             node_level
+    );
+     */
 
     let mut max_dist: i32 = 0;
     let mut penmax_dist: i32 = 0;
-
     for l in &leaves {
-        let mut current = *l;
-        let mut path_len = 0;
-        while parent.contains_key(&current) && !vis.contains(&current) {
-            vis.insert(current);
-            local_dist.insert(
-                current,
-                path_len
-            );
-            path_len += 1;
-            current = parent[&current];
-        }
-
-        println!("current {}", current);
-        println!("current leave-root distance {}",
-                 path_len +
-                 local_dist.get(&current).unwrap_or(&0)
-        );
-
+        // println!("leave {}, level {}", l, node_level[l]);
         // store only the last 2 maximal values
         let mut temp_vec = vec![
-            local_dist[&current] + path_len,
-            penmax_dist, max_dist,
+            node_level[l], penmax_dist, max_dist,
         ];
-
         temp_vec.sort();
         penmax_dist = temp_vec[1];
         max_dist = temp_vec[2];
     }
 
-    println!("{:?}",
-             local_dist
-    );
-
     println!(
-        "answer : {}",
-        penmax_dist + max_dist
+        "{}",
+        (penmax_dist + max_dist) * 3
     );
 }
