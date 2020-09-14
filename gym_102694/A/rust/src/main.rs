6137c6b93e228d2b3c238b9fa19849783b773565
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-15 02:58:33 +0300

    Now able to calculate the center. Even if it is not a single value, but two values. If I remember, it can be shown that one of these two cases always holds true.

--- gym_102694/A/rust/src/main.rs
+++ gym_102694/A/rust/src/main.rs
@@ -15,7 +15,7 @@ fn read_line_of_i32s() -> Vec<i32> {
 fn main() {
     let n = read_line_of_i32s()[0];
     // it ugly but what can i do? i do not have the input in advance.
-    let mut tree: Vec<Vec<i32>> = vec![vec![]; 3 * 100000 + 1];
+    let mut tree: Vec<Vec<i32>> = vec![vec![]; n as usize + 1];
     for i in 0..n - 1 {
         // read the whole line
         let pair = read_line_of_i32s();
@@ -31,7 +31,8 @@ fn main() {
     // Run a bfs and identify leaves
     let mut bfs = std::collections::VecDeque::<i32>::new();
     let mut vis = std::collections::HashSet::<i32>::new();
-    let mut leaves: Vec<i32> = vec![];
+    // will later be used as a queue => therefore VecDeque
+    let mut leaves = std::collections::VecDeque::<i32>::new();
     let mut node_level = std::collections::HashMap::<i32, i32>::new();
     let mut root = -1;
     for (i, val) in tree.iter().enumerate() {
@@ -60,12 +61,10 @@ fn main() {
         for c in &tree[current as usize] {
             if !vis.contains(&c) {
 
-                /*
                 println!(
                     "{} -> {}",
                     current, c
                 );
-                 */
 
                 vis.insert(*c);
                 bfs.push_back(*c);
@@ -75,31 +74,52 @@ fn main() {
         }
 
         if bfs.len() == frozen_len {
-            leaves.push(current);
+            leaves.push_back(current);
         }
-
     }
+    vis = std::collections::HashSet::new();
 
-    println!(
-        "leaves {:?}",
-        leaves
+    if tree[root as usize].len() == 1 {
+        leaves.push_back(
+            root
+        );
+    }
+    println!("leaves {:?}",
+             leaves
     );
-    println!("node_level {:?}",
-             node_level
-    );
-
     let mut max_dist: i32 = 0;
     let mut penmax_dist: i32 = 0;
+    let mut num_visited: i32 = 0;
     for l in &leaves {
-        // println!("leave {}, level {}", l, node_level[l]);
-        // store only the last 2 maximal values
-        let mut temp_vec = vec![
-            node_level[l], penmax_dist, max_dist,
-        ];
-        temp_vec.sort();
-        penmax_dist = temp_vec[1];
-        max_dist = temp_vec[2];
+        vis.insert(*l);
     }
+    num_visited += leaves.len() as i32;
+    println!("---");
+    let mut center: Vec<i32> = vec![];
+    while !leaves.is_empty() {
+        let current = leaves.pop_front().unwrap();
+        println!("current {}", current);
+        println!("bfs {:?}", leaves);
+        for next in &tree[current as usize] {
+            if !vis.contains(&next) {
+                // collect the central nodes
+                if num_visited > n - 3 {
+                    center.push(*next);
+                }
+                vis.insert(*next);
+                num_visited += 1;
+                bfs.push_back(*next);
+                println!("{} -> {}",
+                         current,
+                         next
+                );
+            }
+        }
+    }
+
+    println!("the center {:?}",
+             center
+    );
 
     println!(
         "{}",
