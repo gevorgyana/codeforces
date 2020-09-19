b1733d5abcad5afecea508335b769ce5aad1e8f8
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-19 13:54:56 +0300

    Bruteforce.

--- gym_102694/A/rust/src/main.rs
+++ gym_102694/A/rust/src/main.rs
@@ -15,141 +15,72 @@ fn read_line_of_i32s() -> Vec<i32> {
 fn main() {
     let n = read_line_of_i32s()[0];
     let mut tree: Vec<Vec<i32>> = vec![vec![]; n as usize + 1];
+    let mut nodes = std::collections::HashSet::<i32>::new();
     for i in 0..n - 1 {
         // read the whole line
         let pair = read_line_of_i32s();
         tree[pair[0] as usize].push(pair[1]);
         tree[pair[1] as usize].push(pair[0]);
+        nodes.insert(
+            pair[0]
+        );
+        nodes.insert(
+            pair[1]
+        );
     }
     if n == 1 {
         println!("0");
         return;
     }
-
-    // Run a bfs and identify leaves
-    let mut bfs = std::collections::VecDeque::<i32>::new();
-    let mut vis = std::collections::HashSet::<i32>::new();
-    // will later be used as a queue => therefore VecDeque
-    let mut leaves = std::collections::VecDeque::<i32>::new();
-    let mut root = -1;
-    for (i, val) in tree.iter().enumerate() {
-        if !val.is_empty() {
-            root = i as i32;
-            break;
-        }
-    }
-    bfs.push_back(root);
-    vis.insert(root);
-    while !bfs.is_empty() {
-        let current = bfs.pop_front().unwrap();
-        let frozen_len = bfs.len();
-        for c in &tree[current as usize] {
-            if !vis.contains(&c) {
-                vis.insert(*c);
-                bfs.push_back(*c);
+    let mut best_ans = i32::min_value();
+    for root in nodes {
+        let mut bfs = std::collections::VecDeque::<i32>::new();
+        let mut vis = std::collections::HashSet::<i32>::new();
+        let mut lvl = std::collections::HashMap::<i32, i32>::new();
+        bfs.push_back(root);
+        vis.insert(root);
+        lvl.insert(root, 0);
+        while !bfs.is_empty() {
+            let current = bfs.pop_front().unwrap();
+            let current_lvl = lvl[&current];
+            // println!("current {} : lvl {}", current, current_lvl);
+            for next in &tree[current as usize] {
+                if !vis.contains(next) {
+                    bfs.push_back(*next);
+                    vis.insert(*next);
+                    lvl.insert(
+                        *next,
+                        lvl[&current] + 1
+                    );
+                }
             }
         }
-        if bfs.len() == frozen_len {
-            leaves.push_back(current);
-        }
-    }
-    vis = std::collections::HashSet::new();
-    if tree[root as usize].len() == 1 {
-        // println!("root is a leaf too");
-        leaves.push_back(
-            root
+        /*
+        println!(
+            "{:?}",
+            lvl
         );
-    }
-    /*
-    println!("leaves {:?}",
-             leaves
-    );
-     */
-
-    // second pass
-    // find the center, then move from the center to the leaves and
-    // measure the distance, then multiply it by 2
-
-    let mut bfs = std::collections::VecDeque::<i32>::new();
-    let mut central_nodes: Vec::<i32> = vec![];
-    let mut bfs_level = std::collections::HashMap::<i32, i32>::new();
-    for leaf in &leaves {
-        bfs.push_back(*leaf);
-        vis.insert(*leaf);
-        bfs_level.insert(
-            *leaf, 0
-        );
+         */
+        /*
+        let max_level_per_simple_algorithm
+            = lvl.values().max();
+        println!("{}", *max_level_per_simple_algorithm.unwrap() * 3);
+         */
+        best_ans = best_ans.max(*lvl.values().max().unwrap() * 3);
     }
 
-    // collect central nodes
-    while !bfs.is_empty() {
-        let current = bfs.pop_front().unwrap();
-        for next in &tree[current as usize] {
-            if vis.contains(next) {
-                continue
-            }
-            bfs.push_back(*next);
-            vis.insert(*next);
-            bfs_level.insert(
-                *next,
-                bfs_level[&current] + 1
-            );
-        }
-    }
-
-    // collect the central nodes
-    let max_level
-        = bfs_level.values().max().unwrap();
-    for i in &bfs_level {
-        if i.1 == max_level {
-            central_nodes.push(*i.0);
-        }
-    }
-
-    assert!(central_nodes.len() < 3);
-
-    /*
     println!(
-        "central nodes {:?}",
-        central_nodes
+        "{}",
+        best_ans
     );
+
+    /*
+    the diameter is the longest path between any 2 nodes
+    we can use the defininton. take every node, and run bfs from it
+    so in N ^ 2 time we will find the answer by contantly updating it.
+    we could also find just the central nodes, and from them walk all
+    the way down the tree. would it work? if we have a center and we run
+    from the center, then it should work. let's check that.
      */
 
-    // now try to just run a bfs and see the maximum level that you can
-    // get - and assert that your solution does the same.
-    // println!("---");
-    let mut bfs = std::collections::VecDeque::<i32>::new();
-    let mut vis = std::collections::HashSet::<i32>::new();
-    let mut lvl = std::collections::HashMap::<i32, i32>::new();
-    bfs.push_back(root);
-    vis.insert(root);
-    lvl.insert(root, 0);
-    while !bfs.is_empty() {
-        let current = bfs.pop_front().unwrap();
-        for next in &tree[current as usize] {
-            if !vis.contains(next) {
-                bfs.push_back(*next);
-                vis.insert(*next);
-                lvl.insert(
-                    *next,
-                    lvl[&current] + 1
-                );
-            }
-        }
-    }
-    let mut ans: i32 = 0;
-    if central_nodes.len() == 1 {
-        ans = (max_level * 2) * 3;
-    } else {
-        ans = (max_level * 2 + 1) * 3;
-    }
-    // compare with the max level that you obtained from the other
-    // algorithm - it can be optimized, but nevermind
-    let max_level_per_simple_algorithm
-        = lvl.values().max();
-    assert!(/*ans == *max_level_per_simple_algorithm.unwrap() * 3
-            &&*/
-            (ans - *max_level_per_simple_algorithm.unwrap() * 3).abs() < 100000
-    );
-    println!("{}", ans);
 }
